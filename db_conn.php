99ebf1418f98e36e6850b25a649c9c370038358d
Johnmel <manongdo.johnmel@gmail.com>
2022-04-02 00:45:37 +0800

    init

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,14 @@
 <?php
 
-$sname = "localhost";
-$uname = "root";
-$password = "";
+// $sname = "localhost";
+// $uname = "root";
+// $password = "";
+// $db_name = "dbadmin";
 
-$db_name = "dbadmin";
+$sname = "remotemysql.com";
+$uname = "V3ndi3eExB";
+$password = "ZJZC97BCe5";
+$db_name = "V3ndi3eExB";
 
 $conn  = mysqli_connect($sname, $uname, $password, $db_name);
 
