44fd5adf0b05a27c9654fe6d8fc830dde6d8150e
Johnmel <manongdo.johnmel@gmail.com>
2022-04-02 01:07:37 +0800

    init

--- change_pass.php
+++ change_pass.php
@@ -20,6 +20,13 @@ body {
     letter-spacing: 0.05rem;
     padding: 0.75rem 1rem;
 }
+
+.center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+}
 </style>
 
 <body>
@@ -29,48 +36,47 @@ body {
                 <div class="card border-0 shadow rounded-3 my-5">
                     <div class="card-body p-4 p-sm-5">
                         <img src="assets/img/logo.png" alt="logo" class="center" width="150px" height="150px">
-
                         <h5 class="card-title text-center mb-5 fw-light fs-5">PHINMA UPang Navigation Map Admin Panel
-
-                            <?php if (isset($_GET['error'])) { ?>
-                            <div class="alert alert-danger alert-dismissible fade show">
-                                <p class="error"><strong><?php echo $_GET['error']; ?></strong></p>
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                        </h5>
+                        <?php if (isset($_GET['error'])) { ?> <div
+                            class="alert alert-danger alert-dismissible fade show">
+                            <p class="error"><strong><?php echo $_GET['error']; ?></strong></p>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                        </div>
+                        <?php } ?>
+                        <?php if (isset($_GET['success'])) { ?>
+                        <script>
+                        swal({
+                            title: "<?php echo $_GET['success']; ?>",
+                            icon: "success",
+                            button: "Okay",
+                        });
+                        </script>
+                        <?php } ?>
+                        <form action="php/password.php" method="POST">
+                            <div>
+                                <a href="home.php"><i class="fa fa-home fa-2x mb-3"></i></a>
                             </div>
-                            <?php } ?>
-                            <?php if (isset($_GET['success'])) { ?>
-                            <script>
-                            swal({
-                                title: "<?php echo $_GET['success']; ?>",
-                                icon: "success",
-                                button: "Okay",
-                            });
-                            </script>
-                            <?php } ?>
-                            <form action="php/password.php" method="POST">
-                                <div>
-                                    <a href="home.php"><i class="fa fa-home fa-2x mb-3"></i></a>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" name="op" id="floatingInput"
-                                        placeholder="Enter username here...">
-                                    <label for="floatingInput">Old Password</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" name="np" id="floatingPassword"
-                                        placeholder="Enter password here...">
-                                    <label for="floatingPassword">New Password</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" name="cp" id="floatingPassword"
-                                        placeholder="Enter password here...">
-                                    <label for="floatingPassword">Confirm Password</label>
-                                </div>
-                                <div class="d-grid">
-                                    <button onclick="checker()" class="btn btn-success btn-login text-uppercase fw-bold"
-                                        type="submit">Change Password</button>
-                                </div>
-                            </form>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" name="op" id="floatingInput"
+                                    placeholder="Enter username here...">
+                                <label for="floatingInput">Old Password</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" name="np" id="floatingPassword"
+                                    placeholder="Enter password here...">
+                                <label for="floatingPassword">New Password</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" name="cp" id="floatingPassword"
+                                    placeholder="Enter password here...">
+                                <label for="floatingPassword">Confirm Password</label>
+                            </div>
+                            <div class="d-grid">
+                                <button onclick="checker()" class="btn btn-success btn-login text-uppercase fw-bold"
+                                    type="submit">Change Password</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
